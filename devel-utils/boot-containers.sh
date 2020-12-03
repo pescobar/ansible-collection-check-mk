02f5bd55bdab435aa747931abc140eac56f5ddf7
Pablo Escobar <pescobar001@gmail.com>
2020-12-03 12:53:14 +0100

    remove -ti in the docker exec command

--- devel-utils/boot-containers.sh
+++ devel-utils/boot-containers.sh
@@ -3,4 +3,4 @@
 docker-compose up -d
 
 # update login for automation user to "automation:automation"
-docker exec -ti check-mk-ci bash -c "until [ -f /opt/omd/sites/cmk/var/check_mk/web/automation/automation.secret ]; do sleep 1; done; echo automation > /opt/omd/sites/cmk/var/check_mk/web/automation/automation.secret"
+docker exec check-mk-ci bash -c "until [ -f /opt/omd/sites/cmk/var/check_mk/web/automation/automation.secret ]; do sleep 1; done; echo automation > /opt/omd/sites/cmk/var/check_mk/web/automation/automation.secret"
